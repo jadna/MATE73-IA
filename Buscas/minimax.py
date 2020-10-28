a879439eb2f9391ae819b088d654df732d66ebc4
jadna <jadna.ac@gmail.com>
2020-10-27 22:26:38 -0300

    minimax

--- Buscas/minimax.py
+++ Buscas/minimax.py
@@ -1,5 +1,21 @@
 from collections import defaultdict
 
+# Toggle True => Max, False=> Min
+def minimax(graph, folhas, node, toggle):
+
+    valores = []
+    if(node in folhas):
+        return folhas[node]
+
+    for i in graph[node] :
+        valores.append(minimax(graph, folhas, i, not toggle))
+
+    if(toggle):
+        valor_minmax = max(valores)
+    else:
+        valor_minmax = min(valores)    
+
+    return valor_minmax
 
 entry = input().rstrip()
 nodes, rel = entry.split(' ')
@@ -8,14 +24,15 @@ nodes = int(nodes)
 
 #graph = {}
 graph = defaultdict(list)
-folhas = defaultdict(list)
+#folhas = defaultdict(list)
+folhas = {}
 
 for i in range(rel):
     entry = input().rstrip()
     a, b = entry.split(' ')
 
     graph[a].append(b)
-    folhas[b]
+    folhas[b] = None
 
 
 n = input().rstrip()
@@ -26,10 +43,10 @@ for i in range(n):
     a, b = entry.split(' ')
 
     if(a in folhas):
-        folhas[a].append(b)
+        folhas[a] = int(b)
 
 # Elimina as folhas vazias
-folhas = { key: folhas[key] for key in folhas if folhas[key] != [] }
+folhas = { key: folhas[key] for key in folhas if folhas[key] != None }
 
-print(graph)
-print(folhas)
+resultado = minimax(graph, folhas, '1', True)
+print(resultado)
